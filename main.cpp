48e982b35fe8e9899de5faff256a1606092f81a1
Ivan.Yarkov <ivan.yarkov@outlook.com>
2021-07-10 14:03:06 +0300

    delete unused code

--- main.cpp
+++ main.cpp
@@ -63,33 +63,6 @@ public:
     bool OnUserCreate() override {
         // TODO: For future, check if orientation is right wise clock
         mesh cargoShips;
-//        cargoShips.triangles = {
-//
-//                // SOUTH
-//                {0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f},
-//                {0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f},
-//
-//                // EAST                                                      
-//                {1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f},
-//                {1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f},
-//
-//                // NORTH                                                     
-//                {1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f},
-//                {1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f},
-//
-//                // WEST                                                      
-//                {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f},
-//                {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f},
-//
-//                // TOP                                                       
-//                {0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f},
-//                {0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f},
-//
-//                // BOTTOM                                                    
-//                {1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f},
-//                {1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f},
-//
-//        };
         cargoShips.LoadFrom("cat.obj");
         myMeshes.push_back(cargoShips);
 
